e83fdb20f1431641545716806401703f87941f4b
Chris Hogan <chrishogan99@hotmail.com>
2019-08-24 21:13:36 -0500

    Historical Testing

---
+++ Historical-Prediction-Model.py
@@ -0,0 +1,73 @@
+import csv
+import os
+
+def change_directory(folder):
+    #Change Databse Directory
+    dirpath = os.getcwd()
+    dirpath = dirpath + folder
+    os.chdir(dirpath)
+
+def database(path, item_list):
+    #Writes Players to CSV file
+    with open(path + '.csv', 'a', newline='') as file:
+        wr = csv.writer(file, dialect='excel')
+        wr.writerow(item_list)
+
+def database_reader(current_file, head_list):
+    #Read Database Files
+    database_players = []
+    with open(current_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        #Reads rows of CSV file
+        for row in reader:
+            index = 0
+            player_list = []
+            #Sets row to proper information
+            while index < len(row):
+                player_list.append(row[head_list[index]])
+                index += 1
+            database_players.append(player_list)
+    return(database_players)
+
+#Stores old directory
+first_directory = os.getcwd()
+
+for mult in range(10,21,1):
+    mult = mult / 1000
+    os.chdir(first_directory)
+    #Headings
+    spread_head = ['Year', 'Week', 'Home', 'Away', 'Spread', 'Total']
+    win_head = ['Year','Week', 'Team', 'Wins']
+
+    #Create lists of database
+    change_directory('/Database/')
+    spread_list = database_reader('Spread-Database.csv', spread_head)
+    os.chdir(first_directory)
+    change_directory('/Historical-Test/')
+    win_list = database_reader('Weekly-Win-Total-Model-' + str(mult)[2:] + '.csv', win_head)
+
+
+    head = ['Year','Week','Home','Spread','Home Wins','Away','Away Wins','Advantage','ADV Team']
+    database('Prediction-Model-' + str(mult)[2:], head)  
+
+    for year in range(2011, 2019):
+        for week in range(1, 18):
+            for game in spread_list:
+                if game[0] == str(year) and game[1] == str(week):
+                    home_score = 0
+                    away_score = 0
+                    for team in win_list:
+                        if team[0] == str(year) and team[1] == str(week):
+                            if team[2] == game[2]:
+                                home_score = team[3]
+                            elif team[2] == game[3]:
+                                away_score = team[3]
+                    adj_spread = ((float(away_score) - float(home_score))*2)-3
+                    advantage = float(game[4]) - adj_spread
+                    head = [year,week,game[2],game[4],home_score,game[3],away_score,advantage,'-']
+                    if advantage < 0:
+                        head[8] = game[3]
+                    elif advantage > 0:
+                        head[8] = game[2]
+                    database('Prediction-Model-' + str(mult)[2:], head)
+
---
+++ Historical-Test.py
@@ -0,0 +1,83 @@
+import csv
+import os
+
+def change_directory(folder):
+    #Change Databse Directory
+    dirpath = os.getcwd()
+    dirpath = dirpath + folder
+    os.chdir(dirpath)
+
+def database(path, item_list):
+    #Writes Players to CSV file
+    with open(path + '.csv', 'a', newline='') as file:
+        wr = csv.writer(file, dialect='excel')
+        wr.writerow(item_list)
+
+def database_reader(current_file, head_list):
+    #Read Database Files
+    database_players = []
+    with open(current_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        #Reads rows of CSV file
+        for row in reader:
+            index = 0
+            player_list = []
+            #Sets row to proper information
+            while index < len(row):
+                player_list.append(row[head_list[index]])
+                index += 1
+            database_players.append(player_list)
+    return(database_players)
+
+#Stores old directory
+first_directory = os.getcwd()
+change_directory('/Historical-Test/')
+final = ['First Year','Last Year','Multiplier','Range','Wins','Loses','Push','W%']
+database('Final-Test', final)
+
+for mult in range(10,22,1):
+    os.chdir(first_directory)
+    mult = mult / 1000
+    
+    #Headings
+    prediction_head = ['Year','Week','Home','Spread','Home Wins','Away','Away Wins','Advantage','ADV Team']
+    boxscore_head = ['Year', 'Week', 'Home', 'Home Score', 'Away', 'Away Score', 'ID']
+
+    #Create lists of database
+    change_directory('/Database/')
+    boxscore_list = database_reader('Boxscore-Database.csv', boxscore_head)
+    os.chdir(first_directory)
+    change_directory('/Historical-Test/')
+    prediction_list = database_reader('Prediction-Model-' + str(mult)[2:] +'.csv', prediction_head)
+
+    head = ['Year','Week','Home','Spread','Home Wins','Away','Away Wins','Advantage','ADV Team']
+    #database('Prediction-Model', head)  
+
+    for year in range(2011,2019):
+        for adv in range(0,15):
+            ats_w = 0
+            ats_l = 0
+            ats_p = 0
+
+            for game in prediction_list:
+                final_score = 0
+                for score in boxscore_list:
+                    if game[0] == score[0] and game[1] == score[1] and game[2] == score[2] and int(game[0]) >= year:
+                        final_score = (float(score[3]) + float(game[3])) - float(score[5])
+                        if float(game[7]) > adv or float(game[7]) < -adv:
+                            if float(final_score) > 0:
+                                if game[2] == game[8]:
+                                    ats_w +=1
+                                else:
+                                    ats_l +=1
+                            elif float(final_score) < 0:
+                                if game[5] == game[8]:
+                                    ats_w +=1
+                                else:
+                                    ats_l +=1
+                            elif float(final_score) == 0:
+                                ats_p += 1
+                                
+            if ats_w + ats_l != 0:
+                final = [year,2018,mult,adv,ats_w,ats_l,ats_p,(ats_w / (ats_w + ats_l)) * 100]
+                database('Final-Test', final)
